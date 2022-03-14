742edb733499b29e260721fc1486b9be322308d5
bigfarts <bigfarts@punymail.com>
2022-03-14 15:08:07 -0700

    allow wrapping streams without loading

--- beep.go
+++ beep.go
@@ -71,5 +71,9 @@ func Load(in io.ReadSeekCloser) (beep.StreamCloser, beep.Format, Info, error) {
 		return stream, format, info, nil
 	}
 
-	return &streamerAndCloser{beep.Seq(beep.Take(int(info.LoopStart), stream), beep.Loop(-1, &interval{stream, int(info.LoopStart), int(info.LoopStart + info.LoopLength)})), stream}, format, info, nil
+	return Wrap(stream, info), format, info, nil
+}
+
+func Wrap(stream beep.StreamSeekCloser, info Info) beep.StreamCloser {
+	return &streamerAndCloser{beep.Seq(beep.Take(int(info.LoopStart), stream), beep.Loop(-1, &interval{stream, int(info.LoopStart), int(info.LoopStart + info.LoopLength)})), stream}
 }
